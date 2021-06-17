cd1f108b13abbf84621d734de154d2d6f227b121
Yuliia Burko <yuliia.burko@gmail.com>
2021-06-17 21:01:27 +0200

    fixed form reset

--- src/Components/ContactForm/ContactForm.js
+++ src/Components/ContactForm/ContactForm.js
@@ -47,7 +47,7 @@ class ContactForm extends Component {
             className={s.form_input}
             type="text"
             name="name"
-            value={this.name}
+            value={this.state.name}
             onChange={this.handleInputChange}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
@@ -60,7 +60,7 @@ class ContactForm extends Component {
             className={s.form_input}
             type="tel"
             name="number"
-            value={this.number}
+            value={this.state.number}
             onChange={this.handleInputChange}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
